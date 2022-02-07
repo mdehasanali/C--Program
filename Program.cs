30bbf0007d7b2a89a3ba8aa07b8f2c16104e2c0d
Md. Ehasan Ali <mdehasanali100@gmail.com>
2022-02-07 11:50:49 +0600

    Frist Program run

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace DataType
+{
+    class Program 
+    {
+
+        static void Main(string[] arrays){
+            Console.WriteLine("My name is Ehasan");
+        }
+    }
+}
