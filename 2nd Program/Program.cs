bfb643814be0087a93e9195f82c99ae4d848afab
Md. Ehasan Ali <mdehasanali100@gmail.com>
2022-02-07 12:15:36 +0600

    2nd Program on c#

---
+++ 2nd Program/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace DataType
+{
+    class Program
+    {
+        static void Main(string[] arrays){
+            int myNumber = 10;
+            double myBoubleNumber = 9.55;
+            char myChar = 'D';
+            bool myBool = true;
+            string myString = "My name is Ehasan Ali";
+
+            System.Console.WriteLine(myNumber);
+            System.Console.WriteLine(myBoubleNumber);
+            System.Console.WriteLine(myChar);
+            System.Console.WriteLine(myBool);
+            System.Console.WriteLine(myString);
+
+        }
+    }
+}
