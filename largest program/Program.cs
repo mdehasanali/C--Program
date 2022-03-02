0bd67c1eacec5b313aea4b215f79ad2b3d5536e5
Md. Ehasan Ali <mdehasanali100@gmail.com>
2022-03-02 11:50:27 +0600

    largest program code in c#

--- largest program/Program.cs
+++ largest program/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace LargestNumber
+{
+    class Program
+    {
+        static void Main(string[] arrys)
+        {
+
+            int first, secend, third;
+            Console.WriteLine("Inter the First number");
+            first = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Inter the Secend number");
+            secend = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Inter the Thrid number");
+            third = Convert.ToInt32(Console.ReadLine());
+
+            if (first > secend && first > third)
+            {
+                Console.WriteLine("The Largest Number - " + first);
+            }
+            else if (secend > first && secend > third)
+            {
+                Console.WriteLine("The Largest Number - " + secend);
+            }
+            else
+            {
+                Console.WriteLine("The Largest Number - " + third);
+            }
+            Console.WriteLine("The Code is End");
+        }
+    }
+}
