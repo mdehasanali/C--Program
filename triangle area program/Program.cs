807136dd071182ff767657ca3ba17b256ac7e234
Md. Ehasan Ali <mdehasanali100@gmail.com>
2022-03-02 12:46:49 +0600

    Triangle area program update

--- triangle area program/Program.cs
+++ triangle area program/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] arrays)
+        {
+            double a, b, c, s, area;
+
+            System.Console.WriteLine("Enter The First Number");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            System.Console.WriteLine("Enter The Sceand Number");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            System.Console.WriteLine("Enter The Thrid Number");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            if ((a + b) > c && (b + c) > a && (a + c) > b)
+            {
+                s = (a + b + c) / 2;
+                area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+                Console.WriteLine("Triangle area is - " + area);
+            }
+            else
+            {
+                Console.WriteLine("Invaild Number");
+            }
+            System.Console.WriteLine("Triangle Program is End !");
+        }
+    }
+}
