e16d0ca31edf9df7b6f41b1a8468dd10d322618d
Md. Ehasan Ali <mdehasanali100@gmail.com>
2022-03-02 12:07:52 +0600

    Smallest program code update

--- smallest number program/Program.cs
+++ smallest number program/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Smallest
+{
+    class Program
+    {
+        static void Main(string[] arrays)
+        {
+            int first, secend, thrid;
+
+            System.Console.WriteLine("Enter the First Number");
+            first = Convert.ToInt32(System.Console.ReadLine());
+
+            System.Console.WriteLine("Enter the secend Number");
+            secend = Convert.ToInt32(System.Console.ReadLine());
+
+            System.Console.WriteLine("Enter the thrid Number");
+            thrid = Convert.ToInt32(System.Console.ReadLine());
+
+            if (first < secend)
+            {
+                if (first < thrid)
+                {
+                    System.Console.WriteLine("The Smallest Number - " + first);
+                }
+                else
+                {
+                    System.Console.WriteLine("The Smallest Number - " + thrid);
+                }
+            }
+
+            else
+            {
+                if (secend < thrid)
+                {
+                    System.Console.WriteLine("The Smallest Number - " + secend);
+                }
+                else
+                {
+                    System.Console.WriteLine("The Smallest Number - " + thrid);
+                }
+            }
+            Console.WriteLine("The Program is End");
+        }
+    }
+}
