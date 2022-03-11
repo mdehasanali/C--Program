bde7b06b9c57cd0492654d60a105f150da1499b7
Md. Ehasan Ali <mdehasanali100@gmail.com>
2022-03-11 12:16:49 +0600

    hello world program add  and folder name change

---
+++ 1 Hello-world Program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace String
+{
+    class Program
+    {
+
+        static void Main(string[] arrays)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
