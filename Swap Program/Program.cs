f8473b3a19cd254e83d78f77fb0251c1a7071cca
Md. Ehasan Ali <mdehasanali100@gmail.com>
2022-03-11 14:56:21 +0600

    Swap Program update

--- Swap Program/Program.cs
+++ Swap Program/Program.cs
@@ -6,7 +6,19 @@ namespace swap
 
         public static void Main(string[] arrays)
         {
+            int temp;
 
+            System.Console.WriteLine("Enter A Number - ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            System.Console.WriteLine("Enter B Number - ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+
+            System.Console.WriteLine("value of num1 and num2 is :- " + (num1, num2));
         }
     }
 }
