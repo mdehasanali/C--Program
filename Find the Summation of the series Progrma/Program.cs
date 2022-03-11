b9b0cc984dae7b737a160b53a487f22ac756d61b
Md. Ehasan Ali <mdehasanali100@gmail.com>
2022-03-11 14:34:22 +0600

    Find the Summation of the series Program update

--- Find the Summation of the series Progrma/Program.cs
+++ Find the Summation of the series Progrma/Program.cs
@@ -5,8 +5,26 @@ namespace summation
     {
 
         public static void Main(string[] arrays)
-        {
 
+        {
+            int sum = 0;
+            for (int n = 1; n <= 100; n++)
+            {
+                sum = sum + n;
+            }
+            System.Console.WriteLine(sum);
+
+
+
+
+            // int n = 1;
+            // int sum = 0;
+            // while (n <= 100)
+            // {
+            //     sum = sum + n;
+            //     n = n + 1;
+            // }
+            // System.Console.WriteLine(sum);
         }
     }
 }
