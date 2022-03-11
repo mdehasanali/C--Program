f919d03429421544a6ce87d393fee36cef5a7d63
Md. Ehasan Ali <mdehasanali100@gmail.com>
2022-03-11 14:19:22 +0600

    Centigrade to farenheight Program Update

--- Centigrade to farenheight Program/Program.cs
+++ Centigrade to farenheight Program/Program.cs
@@ -6,7 +6,13 @@ namespace Centigrade
 
         public static void Main(string[] arrays)
         {
+            int number, centigrade;
+            System.Console.WriteLine("Enter Fahrenheit temperature: ");
+            number = Convert.ToInt32(Console.ReadLine());
 
+            centigrade = ((number - 32) * 5) / 9;
+
+            System.Console.WriteLine("Centigrade value is:- " + centigrade);
         }
     }
 }
