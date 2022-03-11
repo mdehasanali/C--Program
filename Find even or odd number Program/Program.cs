96111db80cb8a8597baf9b2b50cb5c0406fe11d4
Md. Ehasan Ali <mdehasanali100@gmail.com>
2022-03-11 11:41:37 +0600

    Find even or odd number Program update

--- Find even or odd number Program/Program.cs
+++ Find even or odd number Program/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Cricle
+{
+    class Program
+    {
+        public static void Main(string[] arrays)
+        {
+            int number;
+
+            System.Console.WriteLine("Enter the Number -");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            if (number == 0)
+            {
+                System.Console.WriteLine("ZERO Number is - " + number);
+            }
+            else if (number % 2 == 0)
+            {
+                System.Console.WriteLine("The Odd Number is - " + number);
+            }
+            else
+            {
+                System.Console.WriteLine("The Even Number is - " + number);
+            }
+        }
+    }
+}
