fed3c4c953b53c907886c3bd3c2a6ab944e19e36
Md. Ehasan Ali <mdehasanali100@gmail.com>
2022-03-11 19:04:02 +0600

    Prime number between 1 to 100 program update

--- Prime number between 1 to 100 program/Program.cs
+++ Prime number between 1 to 100 program/Program.cs
@@ -5,7 +5,29 @@ namespace Prime_number
     {
         static void Main(string[] arrays)
         {
+            System.Console.WriteLine("Prime Number between 1 and 100");
+            System.Console.WriteLine("==================================");
 
+            for (int i = 1; i < 101; i++)
+            {
+                int count_number = 0;
+                if (i > 1)
+                {
+                    for (int j = 2; j < i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            count_number = 1;
+                            break;
+                        }
+
+                    }
+                    if (count_number == 0)
+                    {
+                        System.Console.WriteLine(i);
+                    }
+                }
+            }
         }
     }
 }
