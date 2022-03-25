0194a3759e2e44f5182ae28500f032a83eac7a1e
Md. Ehasan Ali <mdehasanali100@gmail.com>
2022-03-25 17:19:37 +0600

    file update c#

--- Dighat Program/Program.cs
+++ Dighat Program/Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CSharp_Shell
+{
+
+    public static class Program 
+    {
+        public static void Main(string[]arrays) 
+        {
+           int a,b,c;
+           double x1,x2,d;
+           
+			Console.WriteLine("frist number a");
+			a = Convert.ToInt32(Console.ReadLine());
+           
+			Console.WriteLine("secrnd number b");
+			b = Convert.ToInt32(Console.ReadLine());
+          
+			Console.WriteLine("third number c");
+			c = Convert.ToInt32(Console.ReadLine());
+          
+          d=b*b-4*a*c;
+          if(d>0){
+          	x1= -b+Math.Sqrt(d)/(2*a);
+          	x2= -b-Math.Sqrt(d)/(2*a);
+          	Console.WriteLine("The x1 Number is :- " x1);
+          	Console.WriteLine("The x1 Number is :- " x2);
+          }else{
+          	Console.WriteLine("Not posiable");
+          }
+        }
+        
+    }
+}
