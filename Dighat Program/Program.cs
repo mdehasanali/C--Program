16c1c308dc0e785e0dc5fcd52ec31b051ed64117
Md. Ehasan Ali <mdehasanali100@gmail.com>
2022-03-26 07:33:38 +0600

    C# formet and else if added

--- Dighat Program/Program.cs
+++ Dighat Program/Program.cs
@@ -3,35 +3,36 @@ using System.IO;
 using System.Linq;
 using System.Collections.Generic;
 
-namespace CSharp_Shell
-{
+namespace CSharp_Shell {
 
-    public static class Program 
-    {
-        public static void Main(string[]arrays) 
-        {
-           int a,b,c;
-           double x1,x2,d;
-           
-			Console.WriteLine("frist number a");
-			a = Convert.ToInt32(Console.ReadLine());
-           
-			Console.WriteLine("secrnd number b");
-			b = Convert.ToInt32(Console.ReadLine());
-          
-			Console.WriteLine("third number c");
-			c = Convert.ToInt32(Console.ReadLine());
-          
-          d=b*b-4*a*c;
-          if(d>0){
-          	x1= -b+Math.Sqrt(d)/(2*a);
-          	x2= -b-Math.Sqrt(d)/(2*a);
-          	Console.WriteLine("The x1 Number is :- " x1);
-          	Console.WriteLine("The x1 Number is :- " x2);
-          }else{
-          	Console.WriteLine("Not posiable");
-          }
+    class Program {
+        public static void Main(string[] arrays) {
+            int a, b, c;
+            double x1, x2, d;
+
+            Console.WriteLine("frist number a");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("secrnd number b");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("third number c");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            d = b * b - 4 * a * c;
+
+            if (d > 0) {
+                x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                Console.WriteLine("The x1 Number is :- "
+                    x1);
+                Console.WriteLine("The x1 Number is :- "
+                    x2);
+            } else if (d == 0) {
+                x = (-b / (2 * a))
+            } else {
+                Console.WriteLine("Not posiable");
+            }
         }
-        
     }
 }
