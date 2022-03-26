4d1ed1bc27a8fd46bb675b8859edd3a870def75e
Md. Ehasan Ali <mdehasanali100@gmail.com>
2022-03-26 08:44:47 +0600

    data sorting file update

--- Data sorting/Program.cs
+++ Data sorting/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using System;
+namespace DataSort
+{
+    class mainProgram
+    {
+        public static void Main(string[] arrays){
+            int[] arrys =new int[] {10,4,3,8,1,6,7,8,5};
+            int temp = 0;
+
+            for (int i = 0; i < arrys.Length; i++)
+            {
+                for (int j = 0; j < arrys.Length; j++)
+                {
+                    if (arrys[i] < arrys[j])
+                    {
+                        temp = arrys[i];
+                        arrys[i] = arrys[j];
+                        arrys[j] = temp;
+                    }
+                }
+            }
+            for (int i = 0; i < arrys.Length; i++)
+            {
+                System.Console.WriteLine(arrys[i]);
+            }
+        }
+    }
+}
